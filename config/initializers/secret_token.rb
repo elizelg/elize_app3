8d61c615003b6fa668aff54e151ede096a467363
Elize la Grange <elize.lagrange@gmail.com>
2013-04-12 17:27:47 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElizeApp3::Application.config.secret_token = '49be76d565dd7f22c7cc069174aed6b8f3e73db72bd3c0564e912661cb8ac7d069a2e040f5ae89e82e2e72acfb81c1e0ef12df7786268ed9a2f2e47865e797e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ElizeApp3::Application.config.session_store :cookie_store, key: '_elize_app3_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ElizeApp3::Application.config.session_store :active_record_store
